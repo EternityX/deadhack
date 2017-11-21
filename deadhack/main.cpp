fe45d41734264c184445ffe3ebc23506f3b1dd91
melanite <ehex55@gmail.com>
2017-11-21 03:11:56 -0500

    clean up main

--- deadhack/main.cpp
+++ deadhack/main.cpp
@@ -4,7 +4,6 @@
 CSGO    g_csgo{};
 Input   g_input{};
 
-// todo - dex; maybe move this into its own class ( named after the cheat )?
 static ulong_t __stdcall cheat_init( void *arg ) {
     if( !g_csgo.init() ) {
 #ifdef CHEAT_DBG
@@ -34,7 +33,7 @@ static ulong_t __stdcall cheat_free( void *arg ) {
 }
 
 int __stdcall DllMain( HMODULE self, ulong_t reason_for_call, void *reserved ) {
-    HANDLE cheat_thread /*, free_thread*/;
+    HANDLE cheat_thread, free_thread;
 
     if( reason_for_call == DLL_PROCESS_ATTACH ) {
         cheat_thread = CreateThread( nullptr, 0, &cheat_init, nullptr, 0, nullptr );
@@ -43,10 +42,10 @@ int __stdcall DllMain( HMODULE self, ulong_t reason_for_call, void *reserved ) {
 
         // todo - dex; save off handle to our module...?
 
-        //// todo - dex; do we really need an unload routine?
-        //free_thread = CreateThread( nullptr, 0, &cheat_free, nullptr, 0, nullptr );
-        //if( !free_thread )
-        //    return 0;
+        // todo - dex; do we really need an unload routine?
+        free_thread = CreateThread( nullptr, 0, &cheat_free, nullptr, 0, nullptr );
+        if( !free_thread )
+            return 0;
 
         CloseHandle( cheat_thread );
 
